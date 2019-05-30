7215b69977fc7ad14bfead1c340f0088ec0b088e
David Suwal <sudavidwal@gmail.com>
2019-05-30 23:54:11 +0930

    side nav for mobile added

--- js/java.js
+++ js/java.js
@@ -14,3 +14,20 @@ $("nav div").click(function() {
                $("ul").removeAttr('style');
          }
   });
+
+  $("side-nav div").click(function() {
+           $("side-nav ul").slideToggle();
+           $("searchbar").slideToggle();
+           $("ul ul").css("display", "none");
+     });
+
+     $('side-nav ul li').click(function () {
+          $(this).siblings().find('ul').slideUp();
+         $(this).find('ul').slideToggle();
+     });
+
+     $(window).resize(function() {
+           if($(window).width() > 768) {
+                 $("ul").removeAttr('style');
+           }
+    });
